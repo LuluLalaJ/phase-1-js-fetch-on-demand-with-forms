3d3a0208aeb73f0afece3c33a646e993e3c72a49
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-27 10:24:00 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
+const moviesURL = "http://localhost:3000/movies/"
+
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+  inputForm.addEventListener('submit', event => {
+    event.preventDefault();
+    const input = document.getElementById("searchByID")
+    // input.reset(); 
+
+    fetch(moviesURL + `${input.value}`)
+        .then(response => response.json())
+        .then(movieData => {
+            const movieSection = document.getElementById('movieDetails')
+            const title = movieSection.querySelector('h4')
+            const summary = movieSection.querySelector('p')
+            
+            title.textContent = movieData.title;
+            summary.textContent = movieData.summary;
+        })
+  })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
